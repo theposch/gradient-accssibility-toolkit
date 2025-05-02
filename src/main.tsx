1eeebd6acf93133940fd8e4e2f9215702ca5cf6a
Christian Poschmann <theposch@users.noreply.github.com>
2025-05-02 15:37:22 -0700

    chore: wrap App with ErrorBoundary in main entry

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './pages/App';
 import './index.css';
 import { Toaster } from 'sonner';
+import ErrorBoundary from '@/components/ErrorBoundary';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Toaster position="bottom-center" richColors />
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
 ); 
